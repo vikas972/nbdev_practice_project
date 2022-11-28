0227ab19282f25d6470cf69772a26500c20a4a1c
Vikas Maurya <vikas.maurya@nagarro.com>
2022-11-28 17:38:07 +0530

    Added some files

--- nbdev_practice_project/_modidx.py
+++ nbdev_practice_project/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://vikas972.github.io',
                 'git_url': 'https://github.com/vikas972/nbdev_practice_project',
                 'lib_path': 'nbdev_practice_project'},
-  'syms': {'nbdev_practice_project.core': {'nbdev_practice_project.core.foo': ('core.html#foo', 'nbdev_practice_project/core.py')}}}
+  'syms': { 'nbdev_practice_project.dp': { 'nbdev_practice_project.dp.fact': ('dp.html#fact', 'nbdev_practice_project/dp.py'),
+                                           'nbdev_practice_project.dp.fibo': ('dp.html#fibo', 'nbdev_practice_project/dp.py')},
+            'nbdev_practice_project.intro': {'nbdev_practice_project.intro.add': ('intro.html#add', 'nbdev_practice_project/intro.py')}}}
